4955db99695f9888dabcc0f20294d708030553b9
Dawn nguyen <dawnnguyen2021@gmail.com>
2023-11-18 01:17:50 -0500

    added commits

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// added variables for hours
 var hour9 = $("#hour-9");
 var hour10 = $("#hour-10");
 var hour11 = $("#hour-11");
@@ -7,10 +8,10 @@ var hour14 = $("#hour-14");
 var hour15 = $("#hour-15");
 var hour16 = $("#hour-16");
 var hour17 = $("#hour-17");
-
+// set day of the week, date, and time
 var currentDay = dayjs();
 $('#currentDay').text(currentDay.format('dddd MMM D, YYYY hh:mm a'));
-
+// did an event listener for the save button
 $(".saveBtn").on("click", function (event) {
   var text = $(this).siblings(".description").val();
   var time = $(this).parent().attr("id");

4fe85111c517bbaa56af0a272d05101751c4ce63
Dawn nguyen <dawnnguyen2021@gmail.com>
2023-11-19 22:18:33 -0500

    added code for the colors

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,44 @@
 // added variables for hours
-var hour9 = $("#hour-9");
-var hour10 = $("#hour-10");
-var hour11 = $("#hour-11");
-var hour12 = $("#hour-12");
-var hour13 = $("#hour-13");
-var hour14 = $("#hour-14");
-var hour15 = $("#hour-15");
-var hour16 = $("#hour-16");
-var hour17 = $("#hour-17");
+// var hour9 = $("#hour-9");
+// var hour10 = $("#hour-10");
+// var hour11 = $("#hour-11");
+// var hour12 = $("#hour-12");
+// var hour13 = $("#hour-13");
+// var hour14 = $("#hour-14");
+// var hour15 = $("#hour-15");
+// var hour16 = $("#hour-16");
+// var hour17 = $("#hour-17");
 // set day of the week, date, and time
-var currentDay = dayjs();
-$('#currentDay').text(currentDay.format('dddd MMM D, YYYY hh:mm a'));
-// did an event listener for the save button
-$(".saveBtn").on("click", function (event) {
-  var text = $(this).siblings(".description").val();
-  var time = $(this).parent().attr("id");
-
-  // Save text in local storage
-  localStorage.setItem(time, text);
-
-});
 
 
+$(document).ready(function () {
+  var currentDay = dayjs();
+  $('#currentDay').text(currentDay.format('dddd MMM D, YYYY hh:mm a'));
+  var currentHour = dayjs().hour()
 
+
+  // did an event listener for the save button
+  $(".saveBtn").on("click", function (event) {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    // Save text in local storage
+    localStorage.setItem(time, text);
+  })
+console.log(currentHour)
+
+  $(".time-block").each(function () {
+    var keyList = $(this).attr("id");
+    $(this).children(".description")
+      .val(localStorage.getItem(keyList))
+    var blockHour = parseInt($(this).attr("id").split("-")[1]);
+    if (currentHour > blockHour) {
+      $(this).addClass("past")
+    } else if (currentHour === blockHour) {
+      $(this).addClass("present")
+    } else {
+      $(this).addClass("future")
+    }
+  })
+
+})
 
